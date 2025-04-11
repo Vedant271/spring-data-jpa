6f275bec133c05891a100c8933f4d1ba0a606bc0
Vedant Karale <vedantkarale271@gmail.com>
2025-04-11 22:27:25 +0530

    Java Persistence Query Language Queries

--- src/main/java/com/javaproject/databaseconnectivity/spring_data_jpa/SpringDataJpaApplication.java
+++ src/main/java/com/javaproject/databaseconnectivity/spring_data_jpa/SpringDataJpaApplication.java
@@ -34,16 +34,25 @@ public class SpringDataJpaApplication {
 //		s3.setName("Rakesh");
 //		s3.setMarks(95);
 //
+////		Save Objects
 //		studentRepository.save(s1);
 //		studentRepository.save(s2);
 //		studentRepository.save(s3);
 
+////        Find all records
 //		System.out.println(studentRepository.findAll());
 
-		Optional<Student> student_1 = studentRepository.findById(103);
-		System.out.println(student_1);
-		Optional<Student> student_2 = studentRepository.findById(104);
-		System.out.println(student_2.orElse(null));
+////        Find record by id
+//		Optional<Student> student_1 = studentRepository.findById(103);
+//		System.out.println(student_1);
+//		Optional<Student> student_2 = studentRepository.findById(104);
+//		System.out.println(student_2.orElse(null));
+
+////		Find record by specific parameter
+		System.out.println(studentRepository.findByNameUsingQuery("Rohit"));
+		System.out.println(studentRepository.findByName("Mohit"));
+		System.out.println(studentRepository.findByMarks(80));
+		System.out.println(studentRepository.findByMarksGreaterThan(80));
 	}
 
 }
