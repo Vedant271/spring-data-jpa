c9a159d5a7ef6700dee04c108181163dd8c6741c
Vedant Karale <vedantkarale271@gmail.com>
2025-04-11 23:38:11 +0530

    update delete database record

--- src/main/java/com/javaproject/databaseconnectivity/spring_data_jpa/SpringDataJpaApplication.java
+++ src/main/java/com/javaproject/databaseconnectivity/spring_data_jpa/SpringDataJpaApplication.java
@@ -19,16 +19,16 @@ public class SpringDataJpaApplication {
 		StudentRepository studentRepository = context.getBean(StudentRepository.class);
 
 //		Student s1 = context.getBean(Student.class);
-//		Student s2 = context.getBean(Student.class);
+		Student s2 = context.getBean(Student.class);
 //		Student s3 = context.getBean(Student.class);
 //
 //		s1.setRollNo(101);
 //		s1.setName("Rohit");
 //		s1.setMarks(90);
 //
-//		s2.setRollNo(102);
-//		s2.setName("Mohit");
-//		s2.setMarks(80);
+		s2.setRollNo(102);
+		s2.setName("Mohit");
+		s2.setMarks(70);
 //
 //		s3.setRollNo(103);
 //		s3.setName("Rakesh");
@@ -49,10 +49,14 @@ public class SpringDataJpaApplication {
 //		System.out.println(student_2.orElse(null));
 
 ////		Find record by specific parameter
-		System.out.println(studentRepository.findByNameUsingQuery("Rohit"));
-		System.out.println(studentRepository.findByName("Mohit"));
-		System.out.println(studentRepository.findByMarks(80));
-		System.out.println(studentRepository.findByMarksGreaterThan(80));
+//		System.out.println(studentRepository.findByNameUsingQuery("Rohit"));
+//		System.out.println(studentRepository.findByName("Mohit"));
+//		System.out.println(studentRepository.findByMarks(80));
+//		System.out.println(studentRepository.findByMarksGreaterThan(80));
+
+////		Update and delete data
+		studentRepository.save(s2); // update
+		studentRepository.delete(s2); // delete
 	}
 
 }
